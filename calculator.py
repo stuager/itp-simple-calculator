02199af95f30db6d80087abfed8cc11c91e85b5b
stuager <79288025+stuager@users.noreply.github.com>
2021-02-18 15:10:08 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,26 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    return x / y
 
 
 def multiply(x, y):
-    pass
+    return x * Y
 
 
 def square(x):
-    pass
+    return x ** 2
 
 
 def power(x, y):
-    pass
+    reture x ** y
 
 
 def sqrt(x):
-    pass
+    import math
+    return math.sqrt(x)
+    
